ad92603eb61fa09fe334cdd86e470ccde8ec236d
Yaryna <yaryna.napyivoda@gmail.com>
2019-07-04 19:32:18 +0300

    Change of math formula and final touches

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,19 +1,19 @@
-const strA = prompt("Please enter x1, y1", "");
-const strB = prompt("Please enter x2, y2", "");
-const strC = prompt("Please enter x3, y3", "");
+const strA = prompt("Please enter x, y of point A", "");
+const strB = prompt("Please enter x, y of point B", "");
+const strC = prompt("Please enter x, y of point C", "");
 
-const p1 = strA.split(',');
-const p2 = strB.split(',');
-const p3 = strC.split(',');
+const pA = strA.split(',');
+const pB = strB.split(',');
+const pC = strC.split(',');
 
-const x1 = parseFloat(p1[0]);
-const y1 = parseFloat(p1[1]);
-const x2 = parseFloat(p2[0]);
-const y2 = parseFloat(p2[1]);
-const x3 = parseFloat(p3[0]);
-const y3 = parseFloat(p3[1]);
+const xA = parseFloat(pA[0]);
+const yA = parseFloat(pA[1]);
+const xB = parseFloat(pB[0]);
+const yB = parseFloat(pB[1]);
+const xC = parseFloat(pC[0]);
+const yC = parseFloat(pC[1]);
 
-const l1 = Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
-const l2 = Math.sqrt(((x3-x2) * (x3-x2)) + ((y3-y2) * (y3-y2)));
+const xCen = (xB+xA)/2;
+const yCen = (yB+yA)/2;
 
-console.log(l1===l2);
+console.log(xC===xCen && yC===yCen);
