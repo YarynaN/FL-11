a62054eaa5dcfd3222ed4e81fc5d3ebf7cf02dd6
Yaryna <yaryna.napyivoda@gmail.com>
2019-07-05 17:37:46 +0300

    Step 1 and Step 2

--- FL11_HW7/homework/src/js/task1.js
+++ FL11_HW7/homework/src/js/task1.js
@@ -1,1 +1,16 @@
-// Your code goes here
+const emailEntered = prompt("Enter your email here, would you?", "");
+
+if (emailEntered === null) {
+    alert ("Cancelled");
+} else if (emailEntered.length < 6) {
+    alert("I don't know any emails having name length less than 6 symbols");
+} else if (emailEntered === "user@gmail.com" || emailEntered === "admin@gmail.com"){
+    var password = prompt("Please enter your password, would you?", "");
+        if (password === null) {
+            alert("Canceled.");
+        } else if ( emailEntered !== "user@gmail.com" && password !== "UserPass" || emailEntered !== "admin@gmail.com" && password !== "AdminPass"){
+        alert("Wrong password");
+        }
+} else {
+    alert ("I don’t know you");
+}
