edbedcb3b8447ecbccd45c644b3741cdf9cf2e11
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-07-06 12:14:49 +0300

    Task2 codestyle fixes

--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,23 +1,19 @@
-const strA = prompt("Please enter a", "");
-const strB = prompt("Please enter b", "");
-const strC = prompt("Please enter c", "");
+const strA = prompt('Please enter a', '');
+const strB = prompt('Please enter b', '');
+const strC = prompt('Please enter c', '');
 
 const a = parseFloat(strA);
 const b = parseFloat(strB);
 const c = parseFloat(strC);
 
 if (isNaN(a) || isNaN(b) || isNaN(c) ) {
-    console.log("Invalid number.");
-}
-else if (a > (b+c) || b > (a+c) || c > (a+b)) {
+    console.log('Invalid number.');
+} else if (a > b+c || b > a+c || c > a+b) {
     console.log("This triangle doesn't exist");
-}
-else if(a === b && a === c && b === c){
+} else if(a === b && a === c && b === c) {
     console.log('Equivalent triangle');
-}
-else if(a === b || b === c || a === c){
+} else if(a === b || b === c || a === c) {
     console.log('Isosceles triangle');
-}
-else if (a !== b && a !== c && b !== c){
+} else if (a !== b && a !== c && b !== c) {
     console.log('Normal triangle ');
 }
