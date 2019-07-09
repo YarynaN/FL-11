eea1989dd19c04073b94ba2621ab14a1d0c4d4a5
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-07-09 22:32:27 +0300

    Polishing. Code style fixes. Optimization. Yara is amazing.

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,59 +1,56 @@
-let prize = 0;
-let prize2 = prize * 2;
+const initialMaxSecretNum = 8;
+const initialPrize = 100;
 
-const agree = confirm('Do you want to play a game?');
-if (agree === false) {
-    alert("You did not become a billionaire, but can.");
-} else {
-    let secretNum = Math.floor(Math.random() * 8);
-    let secretNum2 = Math.floor(Math.random() * 12);
-    console.log(secretNum);
-    const prizes = [100, 50, 25];
+const prizeDivider = 2;
+const maxPrizeMultiplier = 2;
+const maxAttempts = 3;
+const secretNumStep = 4;
 
-    for (let j = 0; j < 3; j++) {
+let userBalance = 0;
+let gameNum = 0;
+let maxSecretNum = initialMaxSecretNum;
+let maxPrize = initialPrize;
 
-        for (let i = 0; i < 3; i++) {
-            console.log(i);
-            const guessResponse = prompt('Please, choose a random number between 0 and 8', '');
-            const guess = parseInt(guessResponse);
+let agreedToPlay = confirm('Do you want to play a game?');
+if (!agreedToPlay) {
+    alert('You did not become a billionaire, but can.');
+}
 
-            if (guess === secretNum) {
-                prize = prizes[i];
-                alert("Congratulation, you won! Your prize is: " + prize + "$. Do you want to continue?");
-                // const agreeContinue = confirm('Do you want to continue game?');
-                // if (agreeContinue === false){
-                //     alert("Thank you for your participation. Your prize is: " + prize + "$");
-                }
-                // else {
-                //     for (let i = 0; i < 3; i++) {
-                //         const guessResponse = prompt('Please, choose a random number between 0 and 12', '');
-                //         const guess = parseInt(guessResponse);
-                //         if (guess === secretNum2) {
-                //             prize2 = prizes2[i]+prize;
-                //             alert("Congratulation, you won! Your prize is: " + prize2 + "$. Do you want to continue?");
-                //             break;
-                //         } else {
-                //             alert("Thank you for your participation. Your prize is: " + prize2 + "$");
-                //             const agreeContinue = confirm('Do you want to continue game?');
-                //             if (agreeContinue === false){
-                //                 alert("Thank you for your participation. Your prize is: " + prize2 + "$");
-                //             }
-                //         }
-                //     }
-                // }
-                break;
-            }
-            else {
-                alert("Thank you for your participation. Your prize is: " + prize + "$");
-                // const agreeContinue = confirm('Do you want to continue game?');
-                // if (agreeContinue === false){
-                //     alert("Thank you for your participation. Your prize is: " + prize + "$");
-                // }
-            }
-        const agreeContinue = confirm('Do you want to continue game?');
-        if (agreeContinue === false){
-            alert("Thank you for your participation. Your prize is: " + prize + "$");
+while (agreedToPlay === true) {
+    agreedToPlay = false;
+    let possiblePrize = maxPrize;
+    let secretNum = Math.floor(Math.random() * maxSecretNum);
+
+    for (let attemptsNum = maxAttempts; attemptsNum > 0; attemptsNum--) {
+        const guessResponse = prompt(
+            'Choose a roulette pocket number from 0 to ' + maxSecretNum + '\n' +
+            'Attempts left: ' + attemptsNum + '\n' +
+            'Total prize: ' + userBalance + '$\n' +
+            'Possible prize on current attempt: ' + possiblePrize + '$',
+            ''
+        );
+        const guess = parseInt(guessResponse);
+        if (secretNum === guess) {
+            userBalance += possiblePrize;
+            agreedToPlay = confirm(
+                'Congratulation, you won! Your prize is: ' + userBalance + '$. Do you want to continue?'
+            );
+            break;
         }
+        if (attemptsNum === 1) {
+            userBalance = 0;
+            gameNum = 0;
+            maxSecretNum = initialMaxSecretNum;
+            maxPrize = initialPrize;
         }
+        possiblePrize /= prizeDivider;
+    }
+    if (agreedToPlay) {
+        gameNum++;
+        maxSecretNum += secretNumStep;
+        maxPrize *= maxPrizeMultiplier;
+    } else {
+        alert('Thank you for your participation. Your prize is: ' + userBalance + '$');
+        agreedToPlay = confirm('Do you want to play again? Right?');
     }
 }
