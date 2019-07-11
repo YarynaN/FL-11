b0338ffe62dba9c176027aa4f446e9a9f11264c2
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-07-11 13:30:30 +0300

    Task7

--- FL11_HW8/homework/js/formatTime.js
+++ FL11_HW8/homework/js/formatTime.js
@@ -1,3 +1,11 @@
-let formatTime = (num) => ;
+let formatTime = (num) => {
+    const min = num % 60;
+    num = Math.trunc(num / 60);
+    const hours = num % 24;
+    num = Math.trunc(num / 24);
+    const days = num;
 
-console.log(formatTime());
+    return `${days} day(s) ${hours} hour(s) ${min} minute(s)`;
+};
+
+console.log(formatTime(59));
