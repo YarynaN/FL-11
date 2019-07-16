757c387b37ffe636bc2504642641e3635af6711d
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-07-16 15:40:01 +0300

    Code cleanup

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -9,8 +9,6 @@ let getNumbers = (str) => {
     return nums;
 };
 
-console.log(getNumbers('n1um3ber95'));
-
 //Task 1
 let findTypes = (...args) => {
     let objKeys = {};
@@ -21,16 +19,12 @@ let findTypes = (...args) => {
     return objKeys;
 };
 
-console.log(findTypes(1, null, 15, 'cat', true));
-
 //Task 2
 let executeForEach = (arr, fn) => {
     for (let i = 0; i < arr.length; i++) {
         fn(arr[i]);
     }
 };
-
-executeForEach([1, 2, 3], (el) => console.log(el));
 
 //Task 3
 let mapArray = (arr, fn) => {
@@ -40,10 +34,6 @@ let mapArray = (arr, fn) => {
     });
     return resultArray;
 };
-
-console.log(mapArray([2, 5, 8], (el) => {
-    return el + 3
-}));
 
 //Task 4
 let filterArray = (arr, fn) => {
@@ -55,10 +45,6 @@ let filterArray = (arr, fn) => {
     });
     return resultArray;
 };
-
-console.log(filterArray([2, 5, 8], (el) => {
-    return el > 3
-}));
 
 //Task 5
 let showFormattedDate = (date) => {
@@ -74,8 +60,6 @@ let showFormattedDate = (date) => {
     return `Date: ${monNames[monIndex]} ${day} ${year}`;
 };
 
-console.log(showFormattedDate(new Date('2019-01-27T01:10:00')));
-
 //Task 6
 let canConvertToDate = (strDate) => {
     const date = new Date(strDate);
@@ -84,8 +68,6 @@ let canConvertToDate = (strDate) => {
     let year = date.getFullYear();
     return !Number.isNaN(day) && !Number.isNaN(month) && !Number.isNaN(year);
 };
-
-console.log(canConvertToDate('2016-11-18T00:00:00'));
 
 //Task 7
 let daysBetween = (date1, date2) => {
@@ -98,45 +80,7 @@ let daysBetween = (date1, date2) => {
     return Math.round(difference / millisecondsPerDay);
 };
 
-console.log(daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00')));
-
 //Task 8
-const task8Data = [
-    {
-        '_id': '5b5e3168c6bf40f2c1235cd6',
-        'index': 0,
-        'birthday': '2016-03-18T00:00:00',
-        'eyeColor': 'green',
-        'name': 'Stein',
-        'favoriteFruit': 'apple'
-    },
-    {
-        '_id': '5b5e3168e328c0d72e4f27d8',
-        'index': 1,
-        'birthday': '1991-02-11T00:00:00',
-        'eyeColor': 'blue',
-        'name': 'Cortez',
-        'favoriteFruit': 'strawberry'
-    },
-    {
-        '_id': '5b5e3168cc79132b631c666a',
-        'index': 2,
-        'birthday': '1984-04-17T00:00:00',
-        'eyeColor': 'blue',
-        'name': 'Suzette',
-        'favoriteFruit': 'apple'
-    },
-    {
-        '_id': '5b5e31682093adcc6cd0dde5',
-        'index': 3,
-        'birthday': '1994-04-17T00:00:00',
-        'eyeColor': 'green',
-        'name': 'George',
-        'favoriteFruit': 'banana'
-    }
-];
-
-
 let getAmountOfAdultPeople = (users) => {
     const adultAge = 18;
     const usersFiltered = filterArray(users, el => {
@@ -147,8 +91,6 @@ let getAmountOfAdultPeople = (users) => {
     });
     return usersFiltered.length;
 };
-
-console.log(getAmountOfAdultPeople(task8Data));
 
 //Task 9
 let keys = (obj) => {
@@ -161,8 +103,6 @@ let keys = (obj) => {
     return result;
 };
 
-console.log(keys({keyOne: 1, keyTwo: 2, keyThree: 3}));
-
 //Task 10
 let values = (obj) => {
     let result = [];
@@ -173,5 +113,3 @@ let values = (obj) => {
     }
     return result;
 };
-
-console.log(values({keyOne: 1, keyTwo: 2, keyThree: 3}));
