c8277e2b5663f4c340ff145402c9e894ffca0c2a
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-08-16 21:21:01 +0300

    Task 1

--- FL_11_16_homework_inheritance/homework/js/task1.js
+++ FL_11_16_homework_inheritance/homework/js/task1.js
@@ -1,4 +1,15 @@
 const defaults = { a: 123, b: 777 };
 const options = { a: 456 };
-const configs = assign({}, defaults, options);
 
+
+let assign = function (target, modifier) {
+    for(let i in target){
+        if(Object.prototype.hasOwnProperty.call(modifier, i)){
+            target[i] = modifier[i];
+        }
+    }
+    return target;
+};
+
+const configs = assign(defaults, options);
+console.log(configs);
