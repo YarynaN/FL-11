b024ae2234f3ea57d243ec9d1a24a3ae60ac59b3
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-08-17 17:41:45 +0300

    Task 2

--- FL_11_16_homework_inheritance/homework/js/task2.js
+++ FL_11_16_homework_inheritance/homework/js/task2.js
@@ -1,5 +1,11 @@
 const obj1 = { prop: 5 };
 const obj2 = create(obj1);
 
-Object.getPrototypeOf(obj2) === obj1;
-obj2.prop;
+function create(proto) {
+    function Result() {}
+    Result.prototype = proto;
+    return new Result;
+}
+
+console.log(Object.getPrototypeOf(obj2) === obj1);
+console.log(obj2.prop);
