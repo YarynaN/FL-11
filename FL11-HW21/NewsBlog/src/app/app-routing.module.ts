2561d8c9bd4fa7b595053c7a55cf14cc2c9f4650
Yaryna Mashta <yaryna.nepyivoda@gmail.com>
2019-09-08 13:38:52 +0300

    HTML mockup done

--- FL11-HW21/NewsBlog/src/app/app-routing.module.ts
+++ FL11-HW21/NewsBlog/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CreateNewsComponent } from './create-news/create-news.component';
+import { NewsLineComponent } from './news-line/news-line.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component:  LandingPageComponent},
+  { path: 'create-news', component: CreateNewsComponent },
+  { path: 'news-line', component: NewsLineComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FL11-HW21/NewsBlog/src/app/app.module.ts
+++ FL11-HW21/NewsBlog/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { CreateNewsComponent } from './create-news/create-news.component';
+import { NewsLineComponent } from './news-line/news-line.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {FormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    CreateNewsComponent,
+    NewsLineComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
